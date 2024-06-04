be6100d8ec2a606dc8c71b35813b91226f4e426c
Isac William <hailisac@hotmail.com>
2024-06-04 17:14:40 -0300

    CRUD

---
+++ src/user/user.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Post, Body, Get, Param, Put, Patch, Delete } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body) {
+        return {body};
+    }
+
+    @Get()
+    async list(){
+        return {users: []};
+    }
+
+    @Get(':id')
+    async show(@Param() params) {
+        return {user: {}, params};
+    }
+
+    @Put(':id')
+    async update(@Body() body, @Param() params) {
+        return {
+            method: 'PUT',
+            body,
+            params
+        }
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() params) {
+        return {
+            method: 'PATCH',
+            body,
+            params
+        }
+    }
+
+    @Delete(':id')
+    async delete(@Param() params) {
+        return { params }
+    }
+}
