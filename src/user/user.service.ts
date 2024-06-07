24879201de6e5cf4564e9d1ec21a0ffa104b098e
Isac William <hailisac@hotmail.com>
2024-06-07 11:25:45 -0300

    Middlewares

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,6 +20,8 @@ export class UserService {
     }
     
     async show(id: number){
+        await this.exists(id);
+
         return this.prisma.user.findUnique({
             where: { id }
         });
@@ -82,7 +84,11 @@ export class UserService {
     }
 
     async exists(id: number) {
-        if(!(await this.show(id))) {
+        if(!(await this.prisma.user.count({
+            where: {
+                id
+            }
+        }))) {
             throw new NotFoundException (`O usuário ${id} não existe!`);
         }
     }
