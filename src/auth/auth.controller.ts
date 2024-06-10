85ff94bbda34ee1f2769deebb22fe485d0c63e77
Isac William <hailisac@hotmail.com>
2024-06-10 17:42:13 -0300

    User - Param Decorator customizado

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards} from "@nestjs/common";
 import { AuthLoginDTO } from "./dto/auth-login.dto";
 import { AuthRegisterDTO } from "./dto/auth-register.dto";
 import { AuthForgetDTO } from "./dto/auth-forget.dto";
 import { AuthResetDTO } from "./dto/auth-reset.dto";
 import { UserService } from "src/user/user.service";
 import { AuthService } from "./auth.service";
+import { AuthGuard } from "src/guards/auth.guard";
+import { User } from "src/decorators/user.decorator";
 
 @Controller('auth')
 export class AuthController {
@@ -34,9 +36,9 @@ export class AuthController {
         return this.authService.reset(password, token);
     }
     
+    @UseGuards(AuthGuard)
     @Post('me')
-    async me(@Body() body){
-        return this.authService.checkToken(body.token);
+    async me(@User('email') user){
+        return {user};
     }
-
 }
