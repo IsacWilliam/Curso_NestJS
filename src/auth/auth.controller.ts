aef894292f224c3c4672c9470c62ff42be747999
Isac William <hailisac@hotmail.com>
2024-06-12 18:07:03 -0300

    Upload de Multiplos Arquivos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, UseInterceptors, UploadedFile, BadRequestException} from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, UseInterceptors, UploadedFile, UploadedFiles, BadRequestException} from "@nestjs/common";
 import { AuthLoginDTO } from "./dto/auth-login.dto";
 import { AuthRegisterDTO } from "./dto/auth-register.dto";
 import { AuthForgetDTO } from "./dto/auth-forget.dto";
@@ -7,7 +7,7 @@ import { UserService } from "src/user/user.service";
 import { AuthService } from "./auth.service";
 import { AuthGuard } from "src/guards/auth.guard";
 import { User } from "src/decorators/user.decorator";
-import { FileInterceptor } from "@nestjs/platform-express";
+import { FileInterceptor, FilesInterceptor, FileFieldsInterceptor } from "@nestjs/platform-express";
 import { join } from "path";
 import { FileService } from "src/file/file.service";
 
@@ -60,5 +60,28 @@ export class AuthController {
         }
 
         return {success: true};
+    }
+    
+    @UseInterceptors(FilesInterceptor('files'))
+    @UseGuards(AuthGuard)
+    @Post('files')
+    async uploadFiles(@User() user, @UploadedFiles() files: Express.Multer.File[]){
+        return files;
+    }
+    
+    @UseInterceptors(FileFieldsInterceptor([
+        {
+            name: 'photo',
+            maxCount: 1
+        },
+        {
+            name: 'documents',
+            maxCount: 10
+        }
+    ]))
+    @UseGuards(AuthGuard)
+    @Post('files-fields')
+    async uploadFilesFields(@User() user, @UploadedFiles() files: {photo: Express.Multer.File, documents: Express.Multer.File[]}){
+        return files;
     }
 }
